30d7a673eefdf1438e4b281d5421deaab008c4d3
Nnno1 <159601332+Nnno1@users.noreply.github.com>
2024-11-25 19:18:49 +0800

    Update 111.py

--- mian/111.py
+++ mian/111.py
@@ -4,7 +4,7 @@ print(message)
 #引入海龟库，奇怪的是首次引入会出现全灰，运行后恢复正常引入库的状态
 import turtle as tur
 tur.speed(0)
-#画50个圆圈，但是我也没明白为啥这个样，细见运行结果，速度已被提升
+#画30个圆圈，但是我也没明白为啥这个样，细见运行结果，速度已被提升
 for a in range(30):
     tur.circle(50)
     tur.penup()
