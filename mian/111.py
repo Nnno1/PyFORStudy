ed83d918647c8321b70bf7a57d4b2b720c5f9451
Nnno1 <l1544519785@gmail.com>
2024-11-25 18:58:27 +0800

    第二版本 202411251840

--- mian/111.py
+++ mian/111.py
@@ -1,7 +1,13 @@
 #输出你好世界
+from types import get_original_bases
 
 message='Hello World'
 print('Hello World'+message)
 import turtle as tur
-goto(50,50)
-for a in range(50)
+tur.speed(0)
+for a in range(50):
+    tur.circle(50)
+    tur.penup()
+    tur.fd(100)
+    tur.left(50)
+    tur.pd()
