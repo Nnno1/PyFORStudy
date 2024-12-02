75c53d7769c12d32999b16fd568a69b91b5a989a
Nnno1 <l1544519785@gmail.com>
2024-12-03 02:20:48 +0800

    202412020220

--- mian/结课程序.py
+++ mian/结课程序.py
@@ -1,42 +1,43 @@
 import pandas as pd
 import turtle as t
+
 sample=pd.read_csv('date.csv')
 t.speed(0)
 t.hideturtle()
+
 t.penup()
 t.goto(sample['x'][0], sample['y'][0])
 t.pendown()
-def draw(cw,ch,steplength):
-    t.screensize(canvwidth=cw, canvheight=ch)
-    t.penup()
-    t.goto(-cw/2, 0)
+t.screensize(canvwidth=2000, canvheight=1000)
+
+t.penup()
+t.goto(-1000,0)
+t.pendown()
+t.goto(1000,0)
+t.penup()
+t.goto(0,-1000)
+t.pendown()
+t.goto(0,1000)
+t.penup()
+
+for x1 in range(-500,1000,50):
+    t.goto(x1,-5)
     t.pendown()
-    t.goto(cw/2, 0)
+    t.goto(x1,5)
     t.penup()
-    t.goto(0, -cw/2)
+for y1 in range(-1000,500,50):
+    t.goto(-5,y1)
     t.pendown()
-    t.goto(0, cw/2)
+    t.goto(5,y1)
     t.penup()
-    for x1 in range(-ch/2,cw/2,steplength):
-        t.goto(x1, -5)
-        t.pendown()
-        t.goto(x1, 5)
-        t.penup()
-    for y1 in range(-ch/2,cw/2,steplength):
-        t.goto(-5, y1)
-        t.pendown()
-        t.goto(5,y1)
-        t.penup()
-    t.penup()
-    t.goto(300, 20)
-    t.write("X", font=("Arial", 12, "normal"))
-    t.goto(20, 300)
-    t.write("Y", font=("Arial", 12, "normal"))
-    for i in range(len(sample)):
-        x,y=sample['x'][i], sample['y'][i]
-        t.penup()
-        t.goto(x,y)
-        t.pendown()
-        t.dot(5,'red')
-a,b,c=int(input()),int(input()),int(input())
-draw(a,b,c)
+
+t.goto(300, 20)
+t.write("X", font=("Arial", 12, "normal"))
+t.goto(20, 300)
+t.write("Y", font=("Arial", 12, "normal"))
+
+for i in range(len(sample)):
+    x,y=sample['x'][i],sample['y'][i]
+    t.goto(x,y)
+    t.pendown()
+    t.dot(5,'red')
