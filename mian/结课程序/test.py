60b164c5d75c6e20fa64d213e97f33a1a1295edf
Nnno1 <l1544519785@gmail.com>
2024-12-25 22:27:30 +0800

    test0

--- mian/结课程序/test.py
+++ mian/结课程序/test.py
@@ -14,10 +14,4 @@ elif a==2:
     print(RandomNumber*RandomLittleNumber)
 elif a==3:
     origin,ending=int(input('随机数起点是：')),int(input('随机数末尾是：'))
-    RandomNumber=ra.random(origin,ending)import os
-file_path = 'your_file_path.csv'
-if os.path.exists(file_path):
-# 文件存在，可以进行读取操作
-df = pd.read_csv(file_path)
-else:
-print('文件不存在')
+    RandomNumber=ra.random(origin,ending)
