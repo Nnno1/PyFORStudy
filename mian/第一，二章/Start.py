c31187416bccda2655246ae86752bec7ae787b45
Nnno1 <l1544519785@gmail.com>
2025-01-18 15:00:31 +0800

    结构目录修改

---
+++ mian/第一，二章/Start.py
@@ -0,0 +1,7 @@
+Name=input()
+print('Languages:\n\tPython\n\tC\n\tJave')
+sample=' py thon day '
+user_input='https://python.org'
+print(f'亲爱的{Name},今天一定要来学点Python！\n点击链接 https://python.org')
+print(user_input.removeprefix('https://'))
+print(f'{Name} once said:\n\t"{user_input}"')
---
+++ mian/第三章/3.1.py
@@ -0,0 +1,6 @@
+import random as ra
+import time
+names=['李润','张艺潇','张恒益','杨智文']
+a=ra.randint(0,3)
+print(f'你踏马真帅,{names[a]}')
+time.sleep(999)
