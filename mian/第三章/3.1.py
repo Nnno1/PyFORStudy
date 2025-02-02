0d65976a7f450f4e3cf584a23b910ac259c300ec
Nnno1 <l1544519785@gmail.com>
2025-02-02 15:43:38 +0800

    更新

--- mian/第三章/3.1.py
+++ mian/第三章/3.1.py
@@ -1,3 +1,8 @@
 import random as ra
 names=['李润','张艺潇','张恒益','杨智文','李院']
-print(f'你踏马真帅,{name}')
+names.insert(3,'鸡毛')#三的位置插入某一元素
+print(names)
+names.append('一无是处')#末尾插入一无是处
+print(names)
+del names[3]#删除第三个元素
+print(names)
