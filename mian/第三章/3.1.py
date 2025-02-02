1b29c57cc0bc5f2218948a3a7315c7c0f589e828
Nnno1 <l1544519785@gmail.com>
2025-02-02 16:51:07 +0800

    little change

--- mian/第三章/3.1.py
+++ mian/第三章/3.1.py
@@ -1,8 +1,9 @@
 import random as ra
 names=['李润','张艺潇','张恒益','杨智文','李院']
-names.insert(3,'鸡毛')#三的位置插入某一元素
-print(names)
+#print(names)
 names.append('一无是处')#末尾插入一无是处
-print(names)
+#print(names)
 del names[3]#删除第三个元素
-print(names)
+#print(names)
+for i in range(5):
+    print(f'{names[ra.randint(0,4)]},你最好有点东西')
