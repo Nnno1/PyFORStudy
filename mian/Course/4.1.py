3148b7cdbd108a78f111918514b540f512a88891
Nnno1 <159601332+Nnno1@users.noreply.github.com>
2025-03-21 00:57:14 +0800

    更新 4.1.py

--- mian/Course/4.1.py
+++ mian/Course/4.1.py
@@ -7,7 +7,7 @@ def transform_to_bin(orgin):
         re.append(w)
         if orgin==0:
             break
-        result=''.join(str(i) for i in re)
+    result=''.join(str(i) for i in re)
     return result
 print('程序为转化十进制数值为相应进制数值\n输入举例:5，2\n其中5为需要转化的十进制数字，“2”代表转化为二进制数字')
 print('只能转化为二、八、十六进制数字')
