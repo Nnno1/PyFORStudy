8d5b3a46dac4c8c671867d19e0e3bdcc7dcb9d77
Nnno1 <l1544519785@gmail.com>
2025-03-21 00:48:08 +0800

    进制转化文件

--- mian/Course/4.1.py
+++ mian/Course/4.1.py
@@ -1,8 +1,26 @@
-try:
-    # Safely evaluate input and convert to number
-    user_input = input("请输入一个数字: ")
-    number = float(user_input)  # Convert to float to handle both integers and decimals
-    hex_value = hex(int(number))  # Convert to integer first, then to hex
-    print(f'{number} 对应的十六进制数为 {hex_value}')
-except ValueError:
-    print("输入无效，请输入一个有效的数字")
+def transform_to_bin(orgin):
+    orgin=int(orgin)
+    re=[]
+    while True:
+        w=orgin%2
+        orgin=orgin//2
+        re.append(w)
+        if orgin==0:
+            break
+        result=''.join(str(i) for i in re)
+    return result
+print('程序为转化十进制数值为相应进制数值/n输入举例:“5，2”/n其中5为需要转化的十进制数字，“2”代表转化为二进制数字')
+print('只能转化为二、八、十六进制数字')
+a=eval(input())
+num,aim=a
+if aim==2:
+    result=transform_to_bin(num)
+    print(f'{num}的二进制为{result}')
+elif aim==8:
+    result=str(oct(num))
+    print(f'{num}的八进制为{result}')
+elif aim==16:
+    result=str(hex(num))
+    print(f'{num}的十六进制为{result}')
+else:
+    print("无法转化为相应进制")
---
+++ mian/Course/test_file.py
@@ -0,0 +1,14 @@
+def transform_to_bin(orgin):
+    orgin=int(orgin)
+    re=[]
+    while True:
+        w=orgin%2
+        orgin=orgin//2
+        re.append(w)
+        if orgin==0:
+            break
+        result=''.join(str(i) for i in re)
+    return result
+a=input()
+result=transform_to_bin(a)
+print(result)
