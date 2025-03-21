a0efbf4748f5e25cea7b1549e8cad85bfea06353
Nnno1 <l1544519785@gmail.com>
2025-03-21 13:05:38 +0800

    correctversion

--- mian/Course/4.1.py
+++ mian/Course/4.1.py
@@ -8,6 +8,7 @@ def transform_to_bin(orgin):
         if orgin==0:
             break
     result=''.join(str(i) for i in re)
+    result=re[::-1]
     return result
 print('程序为转化十进制数值为相应进制数值\n输入举例:5，2\n其中5为需要转化的十进制数字，“2”代表转化为二进制数字')
 print('只能转化为二、八、十六进制数字')
@@ -17,10 +18,10 @@ if aim==2:
     result=transform_to_bin(num)
     print(f'{num}的二进制为{result}')
 elif aim==8:
-    result=str(oct(num))
+    result=str(oct(num)).lstrip('0o')
     print(f'{num}的八进制为{result}')
 elif aim==16:
-    result=str(hex(num))
+    result=str(hex(num)).lstrip('0x')
     print(f'{num}的十六进制为{result}')
 else:
     print("无法转化为相应进制")
