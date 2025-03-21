528ae05fe61449580e22ae6fa7da8a5126db407b
Nnno1 <159601332+Nnno1@users.noreply.github.com>
2025-03-21 16:55:40 +0800

    更新 Version.py

--- mian/Version.py
+++ mian/Version.py
@@ -1,2 +1,14 @@
 import sys
 print(sys.version)
+
+
+
+a=input()
+if a.isalpha():
+    print("letter")
+elif a.isdigit():
+    print("digit")
+elif a.isspace():
+    print("space")
+else:
+    print("other")
