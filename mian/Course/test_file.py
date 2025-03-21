562f90f3f8f2ce86e152087fbcbe7f5ec253cc61
Nnno1 <l1544519785@gmail.com>
2025-03-21 12:47:55 +0800

    canrun

--- mian/Course/test_file.py
+++ mian/Course/test_file.py
@@ -7,7 +7,7 @@ def transform_to_bin(orgin):
         re.append(w)
         if orgin==0:
             break
-        result=''.join(str(i) for i in re)
+    result=''.join(str(i) for i in re)
     return result
 a=input()
 result=transform_to_bin(a)
