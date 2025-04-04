0267f9af129c655696f4b1538ed495c923ca483c
Nnno1 <l1544519785@gmail.com>
2025-04-04 12:04:46 +0800

    change

--- mian/homeworks/works4.py
+++ mian/homeworks/works4.py
@@ -7,13 +7,15 @@ from random import randint
 2、控制数据正确性（例如：除法除数不为零，只生成能够整除的题目）
 3、设置关卡升级（例如：先练习单一运算，正确率达到90%以上再进入混合运算练习）
 '''
-
-a,b=randint(0,100),randint(0,100)
-print(f'{a}+{b}=?')
-answer=a+b
-result=int(input())
-
-
+def jia(result):
+    a,b=randint(0,100),randint(0,100)
+    print(f'{a}+{b}=?')
+    answer=a+b
+    result=int(result)
+    if result==answer:
+        return True
+    else:
+        return False
 '''
 if result==answer:
     print('Right!\n')
