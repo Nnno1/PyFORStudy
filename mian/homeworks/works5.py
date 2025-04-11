b7cfcd5568ef70659ffa4dad09ed0b8ea57aeba8
Nnno1 <l1544519785@gmail.com>
2025-04-11 20:37:15 +0800

    work

--- mian/homeworks/works5.py
+++ mian/homeworks/works5.py
@@ -1,16 +1,11 @@
 def huiwen(n):
     if n[::-1]==n:
         return True
-    else:
-        return False    
+    return False
 def sushu(n):
-    if n<=1:
+    if n==0 or n==1:
         return False
-    elif n==2:
-        return True
-    elif n%2==0:
-        return False
-    for i in range(3,int(n**0.5)+1,2):
+    for i in range(2,n):
         if n%i==0:
             return False
     return True
