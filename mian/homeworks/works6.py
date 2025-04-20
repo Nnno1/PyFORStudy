8edd6382b120ef7145d5129c074716e499199bf6
Nnno1 <l1544519785@gmail.com>
2025-04-20 15:26:26 +0800

    success

--- mian/homeworks/works6.py
+++ mian/homeworks/works6.py
@@ -1,9 +1,6 @@
-password=input('请设置密码：')
 shuzi,xiaoxie,daxie,other=0,0,0,0
-total=len(password)
-if total<8:
-    print('密码长度必须大于等于8')
-else:
+def detect(password):
+    global shuzi,xiaoxie,daxie,other
     for i in password:
         if 48<=ord(i)<=57:
             shuzi+=1
@@ -13,17 +10,26 @@ else:
             daxie+=1
         else:
             other+=1
+def feedback(shuzi,xiaoxie,daxie,other):
     sx1,sx2,sx3,sx4='','','',''
     if shuzi==0:
         sx1='数字'
     if xiaoxie==0:
-        sx2='小写'
+        sx2='小写' 
     if daxie==0:
-        sx3='大写'
+        sx3='大写' 
     if other==0:
-        sx4='其他'
-    sx=sx1+sx2+sx3+sx4
-    if sx=='':
-        print('密码设置成功')
+        sx4='其他' 
+    return sx1+sx2+sx3+sx4
+#主程序
+while True:
+    password=input('请设置密码：')
+    if len(password)<8:
+        print('密码至少应该8个字符')
     else:
-        print(f'缺少{sx}字符')
+        detect(password)
+        if feedback(shuzi,xiaoxie,daxie,other)=='':
+            print('密码符合要求')
+            break
+        else:
+            print(f'缺少{feedback(shuzi,xiaoxie,daxie,other)}字符')
