a944d08c933902be9e4f8ac6754962752f456be8
Nnno1 <l1544519785@gmail.com>
2025-05-29 00:33:24 +0800

    ?

--- main/结课程序/第二学期/2.py
+++ main/结课程序/第二学期/2.py
@@ -1,3 +1,22 @@
 import secrets as se
-
-print(se.randbelow(3))
+import random as r
+def a1(total,mode=False):
+    ls,d=[],{0:0,1:0}
+    if mode:
+        for i in range(total):
+            a=r.randint(0,1)
+            ls.append(int(a))
+    else:
+        for i in range(total):
+            a=se.randbelow(2)
+            ls.append(int(a))        
+    for i in ls:
+        if ls[i]==0:
+            d[0]+=1
+        else:
+            d[1]+=1
+    print(f'{(d[0]/total)*100:.2f}',end=',')
+for i in range(5):
+    print(f'{a1(10)}',end=' ')
+for i in range(5):
+    print(f'{a1(10,True)}',end=' ')
