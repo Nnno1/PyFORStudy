6d9469331a5aead21234bf08bac79924b62c4d83
Nnno1 <l1544519785@gmail.com>
2025-08-06 20:10:39 +0800

    learn

---
+++ main/入门到实践/第三章/date0806.py
@@ -0,0 +1,22 @@
+from numpy import random
+alpha_list=[]
+num_list=[]
+
+ALPHA='abcdefghijklmnopqrstuvwxyz'
+ALPHA=ALPHA.upper()
+NUM=range(1,27)
+
+for i,j in zip(ALPHA,NUM):
+    print(i,j)
+
+'''***9*9699969696696*96
+print(random.choice(ALPHA))
+for i in range(1000):
+    num=random.randint(26)
+    num_list.append(str(num))
+
+for i in :
+    alpha_list.insert(i)
+print(alpha_list)
+'''9*66+9-++++699669*69*369
++-++-+--*+9+
