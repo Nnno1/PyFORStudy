703afef3366a2ee7239b8c1d22d45c1987437678
Nnno1 <l1544519785@gmail.com>
2025-08-09 22:57:04 +0800

    08092256

--- main/入门到实践/第三章/date0806.py
+++ main/入门到实践/第三章/date0806.py
@@ -1,22 +1,11 @@
 from numpy import random
-alpha_list=[]
-num_list=[]
 
-ALPHA='abcdefghijklmnopqrstuvwxyz'
-ALPHA=ALPHA.upper()
-NUM=range(1,27)
+ALPHA='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+alpha_list=list(ALPHA)
+new_list=''
 
-for i,j in zip(ALPHA,NUM):
-    print(i,j)
+for i in range(26):
+    res=random.choice(alpha_list)
+    new_list+=res
 
-'''***9*9699969696696*96
-print(random.choice(ALPHA))
-for i in range(1000):
-    num=random.randint(26)
-    num_list.append(str(num))
-
-for i in :
-    alpha_list.insert(i)
-print(alpha_list)
-'''9*66+9-++++699669*69*369
-+-++-+--*+9+
+print(new_list)
